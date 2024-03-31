fedc08853a66d3a93fe19b13816f9a4733ad4dfa
cobot-pc <n/a>
2024-03-31 05:25:25 -0400

    Take intensity into account

--- src/tof_piece_finder.cpp
+++ src/tof_piece_finder.cpp
@@ -1,8 +1,8 @@
-#include <opencv2/calib3d.hpp>
-#include <opencv2/opencv.hpp>
 #include <ament_index_cpp/get_package_share_directory.hpp>
 #include <filesystem>
 #include <limits>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/opencv.hpp>
 
 #include "cv_bridge/cv_bridge.h"
 #include "image_transport/image_transport.hpp"
@@ -79,10 +79,13 @@ public:
     annotated_image_pub_ =
         make_unique<image_transport::Publisher>(it_->advertise(params_->annotated_image_topic, 1));
 
-    // Setup the camera subscriber.
-    auto bound_callback = bind(&TofPieceFinder::image_callback, this, _1, _2);
-    camera_sub_ = make_unique<image_transport::CameraSubscriber>(
-        it_->subscribeCamera(params_->camera_base_topic, 1, bound_callback));
+    // Setup the camera subscribers.
+    const auto depth_callback = bind(&TofPieceFinder::depth_image_callback, this, _1, _2);
+    depth_camera_sub_ = make_unique<image_transport::CameraSubscriber>(
+        it_->subscribeCamera(params_->depth_camera_base_topic, 1, depth_callback));
+    const auto ir_callback = bind(&TofPieceFinder::ir_image_callback, this, _1, _2);
+    ir_camera_sub_ = make_unique<image_transport::CameraSubscriber>(
+        it_->subscribeCamera(params_->ir_camera_base_topic, 1, ir_callback));
 
     RCLCPP_INFO(get_logger(), "Tof Piece Finder node started");
   }
@@ -109,23 +112,14 @@ private:
   }
 
   /**
-   * Callback for the depth image. This function processes the depth image and publishes a sparse
-   * point cloud message, where each point represents a piece on the board. It will also publish
-   * a thresholded image for debugging.
+   * Callback for the depth image.
    *
    * @param[in] image The depth image message.
    * @param[in] cinfo The camera info message.
    */
-  void image_callback(const sensor_msgs::msg::Image::ConstSharedPtr& image,
-                      const sensor_msgs::msg::CameraInfo::ConstSharedPtr& cinfo)
+  void depth_image_callback(const sensor_msgs::msg::Image::ConstSharedPtr& image,
+                            const sensor_msgs::msg::CameraInfo::ConstSharedPtr& cinfo)
   {
-    auto now = rclcpp::Clock().now();
-
-    // Check for parameter updates.
-    if (param_listener_->is_old(*params_)) {
-      params_ = make_unique<Params>(param_listener_->get_params());
-    }
-
     // Convert the image message to a cv::Mat.
     cv_bridge::CvImagePtr cv_ptr;
     try {
@@ -134,32 +128,93 @@ private:
       RCLCPP_ERROR(get_logger(), "cv_bridge exception: %s", e.what());
       return;
     }
+    cv_ptr->image.copyTo(depth_img_);
+    depth_img_stamp_ = rclcpp::Time(image->header.stamp.sec, image->header.stamp.nanosec);
 
-    // Extract important camera parameters.
-    string camera_frame = cinfo->header.frame_id;
-    double fx = cinfo->k[0];  // Focal length in x
-    double fy = cinfo->k[4];  // Focal length in y
-    double cx = cinfo->k[2];  // Optical center in x
-    double cy = cinfo->k[5];  // Optical center in y
+    // Copy camera info.
+    camera_frame_ = cinfo->header.frame_id;
+    fx_ = cinfo->k[0];
+    fy_ = cinfo->k[4];
+    cx_ = cinfo->k[2];
+    cy_ = cinfo->k[5];
 
-    // Apply a gaussian blur to the image.
-    static cv::Mat blurred;
+    if (depth_img_stamp_ == ir_img_stamp_) process_images();
+  }
+
+  /**
+   * Callback for the ir image.
+   *
+   * @param[in] image The ir image message.
+   * @param[in] cinfo The camera info message.
+   */
+  void ir_image_callback(const sensor_msgs::msg::Image::ConstSharedPtr& image,
+                         const sensor_msgs::msg::CameraInfo::ConstSharedPtr& cinfo)
+  {
+    // Convert the image message to a cv::Mat.
+    cv_bridge::CvImagePtr cv_ptr;
+    try {
+      cv_ptr = cv_bridge::toCvCopy(image, sensor_msgs::image_encodings::TYPE_32FC1);
+    } catch (cv_bridge::Exception& e) {
+      RCLCPP_ERROR(get_logger(), "cv_bridge exception: %s", e.what());
+      return;
+    }
+    cv_ptr->image.copyTo(ir_img_);
+    ir_img_stamp_ = rclcpp::Time(image->header.stamp.sec, image->header.stamp.nanosec);
+
+    // Copy camera info.
+    camera_frame_ = cinfo->header.frame_id;
+    fx_ = cinfo->k[0];
+    fy_ = cinfo->k[4];
+    cx_ = cinfo->k[2];
+    cy_ = cinfo->k[5];
+
+    if (depth_img_stamp_ == ir_img_stamp_) process_images();
+  }
+
+  /**
+   * Process the images and publishes a sparse point cloud message, where each point represents a
+   * piece on the board. It will also publish a thresholded image for debugging.
+   */
+  void process_images()
+  {
+    auto now = rclcpp::Clock().now();
+
+    // Check for parameter updates.
+    if (param_listener_->is_old(*params_)) {
+      params_ = make_unique<Params>(param_listener_->get_params());
+    }
+
+    // Apply a gaussian blur to the images.
+    static cv::Mat depth_blurred;
     int blur_size = params_->filter.blur_size;
-    cv::GaussianBlur(cv_ptr->image, blurred, cv::Size(blur_size, blur_size), 0);
+    cv::GaussianBlur(depth_img_, depth_blurred, cv::Size(blur_size, blur_size), 0);
 
     // Apply the gripper mask to the image.
-    cv::Mat masked;
+    cv::Mat depth_masked;
     if (gripper_mask_.data == nullptr)
-      masked = blurred;
+      depth_masked = depth_blurred;
     else
-      masked = blurred.mul(gripper_mask_);
+      depth_masked = depth_blurred.mul(gripper_mask_);
+
+    // Remove all pixels that have too low of an IR intensity.
+    // float min_intensity = numeric_limits<float>::infinity();
+    // float max_intensity = 0;
+    for (int i = 0; i < depth_masked.rows; i++) {
+      for (int j = 0; j < depth_masked.cols; j++) {
+        const float intensity = ir_img_.at<float>(i, j);
+        // if (intensity < min_intensity) min_intensity = intensity;
+        // if (intensity > max_intensity) max_intensity = intensity;
+        if (intensity < 8000) depth_masked.at<float>(i, j) = numeric_limits<float>::infinity();
+      }
+    }
+    // RCLCPP_INFO(node->get_logger(), "Intensity: %0.3f -> %0.3f", min_intensity, max_intensity);
 
     // Create a sorted vector of distances from the image, filtering out the gripper mask and any
     // distances outside the min and max limits.
     vector<float> distances;
-    for (int i = 0; i < masked.rows; i++) {
-      for (int j = 0; j < masked.cols; j++) {
-        float distance = masked.at<float>(i, j);
+    for (int i = 0; i < depth_masked.rows; i++) {
+      for (int j = 0; j < depth_masked.cols; j++) {
+        float distance = depth_masked.at<float>(i, j);
         if (distance < params_->filter.limits.max_distance &&
             distance > params_->filter.limits.min_distance) {
           distances.push_back(distance);
@@ -168,7 +223,7 @@ private:
     }
     sort(distances.begin(), distances.end());
     if (distances.size() < 2) {
-      RCLCPP_WARN(get_logger(), "Cannot find anything");
+      // RCLCPP_WARN(get_logger(), "Cannot find anything");
       return;
     }
 
@@ -183,7 +238,7 @@ private:
     // than the threshold distance. This should remove the chessboard and any other objects that are
     // further away than the pieces.
     cv::Mat binary;
-    cv::threshold(masked, binary, threshold_distance, 255, cv::THRESH_BINARY_INV);
+    cv::threshold(depth_masked, binary, threshold_distance, 255, cv::THRESH_BINARY_INV);
     binary.convertTo(binary, CV_8UC1);
 
     // Find connected components in the binary image. The first component is the background, so we
@@ -202,16 +257,17 @@ private:
       valid_labels.push_back(i);
     }
 
-    // Find the nearest point of every component.
+#if 0
+    // Find the highest point of every component.
     vector<array<int, 2>> highest_points;
     for (size_t i = 0; i < valid_labels.size(); ++i) {
       int label = valid_labels[i];
       float best = numeric_limits<float>::infinity();
       int best_row = 0;
       int best_col = 0;
-      for (int row = 0; row < masked.rows; ++row) {
-        for (int col = 0; col < masked.cols; ++col) {
-          float value = masked.at<float>(row, col);
+      for (int row = 0; row < depth_masked.rows; ++row) {
+        for (int col = 0; col < depth_masked.cols; ++col) {
+          float value = depth_masked.at<float>(row, col);
           if (labels.at<int32_t>(row, col) == label && value < best) {
             best_row = row;
             best_col = col;
@@ -219,34 +275,41 @@ private:
           }
         }
       }
-      highest_points.emplace_back(array<int, 2>({best_col, best_row}));
+      highest_points.emplace_back(array<int, 2>({ best_col, best_row }));
     }
+#endif
 
     // Create a copy of the depth image to draw points on.
     cv::Mat depth_annotated_grey, depth_annotated;
-    cv::normalize(cv_ptr->image, depth_annotated_grey, 0, 255, cv::NORM_MINMAX, CV_8U);
+    cv::normalize(ir_img_, depth_annotated_grey, 0, 255, cv::NORM_MINMAX, CV_8U);
     cv::cvtColor(depth_annotated_grey, depth_annotated, cv::COLOR_GRAY2RGB);
 
     // Create a point cloud message from the connected components.
     int n_points = valid_labels.size();
     pointcloud_msg_.header.stamp = now;
-    pointcloud_msg_.header.frame_id = camera_frame;
+    pointcloud_msg_.header.frame_id = camera_frame_;
     pointcloud_msg_.data.clear();
     pointcloud_msg_.data.reserve(n_points * 12);
     pointcloud_msg_.width = n_points;
     pointcloud_msg_.row_step = n_points * 12;
     for (int i = 0; i < n_points; i++) {
-      int col = highest_points[i][0];
-      int row = highest_points[i][1];
+      const double col_d = centroids.at<double>(valid_labels[i], 0);
+      const double row_d = centroids.at<double>(valid_labels[i], 1);
+      int col = round(col_d);
+      int row = round(row_d);
+      if (col < 0) col = 0;
+      if (row < 0) row = 0;
+      if (col > depth_masked.cols - 1) col = depth_masked.cols - 1;
+      if (row > depth_masked.rows - 1) col = depth_masked.rows - 1;
 
       // Draw point on annotated image.
       cv::circle(depth_annotated, cv::Point2i(col, row), 2, cv::Scalar(255, 0, 0), -1);
 
       // Convert the pixel to a 3D point.
       float point[3];
-      point[0] = -(((cx - col)) / fx) * masked.at<float>(row, col);  // X
-      point[1] = -(((cy - row)) / fy) * masked.at<float>(row, col);  // Y
-      point[2] = masked.at<float>(row, col);                         // Z
+      point[0] = -(((cx_ - col)) / fx_) * depth_masked.at<float>(row, col);  // X
+      point[1] = -(((cy_ - row)) / fy_) * depth_masked.at<float>(row, col);  // Y
+      point[2] = depth_masked.at<float>(row, col);                           // Z
 
       // Add the point to the point cloud message.
       uint8_t* data = reinterpret_cast<uint8_t*>(point);
@@ -259,7 +322,7 @@ private:
     // Publish the thresholded image.
     cv_bridge::CvImage bin_msg;
     bin_msg.header.stamp = now;
-    bin_msg.header.frame_id = camera_frame;
+    bin_msg.header.frame_id = camera_frame_;
     bin_msg.encoding = sensor_msgs::image_encodings::MONO8;
     bin_msg.image = binary;
     binary_image_pub_->publish(bin_msg.toImageMsg());
@@ -267,7 +330,7 @@ private:
     // Publish the annotated image.
     cv_bridge::CvImage ann_msg;
     ann_msg.header.stamp = now;
-    ann_msg.header.frame_id = camera_frame;
+    ann_msg.header.frame_id = camera_frame_;
     ann_msg.encoding = sensor_msgs::image_encodings::RGB8;
     ann_msg.image = depth_annotated;
     annotated_image_pub_->publish(ann_msg.toImageMsg());
@@ -278,8 +341,19 @@ private:
   unique_ptr<image_transport::ImageTransport> it_;  //< Image transport for this node.
   unique_ptr<image_transport::Publisher> binary_image_pub_;
   unique_ptr<image_transport::Publisher> annotated_image_pub_;
-  unique_ptr<image_transport::CameraSubscriber> camera_sub_;
+  unique_ptr<image_transport::CameraSubscriber> depth_camera_sub_;
+  unique_ptr<image_transport::CameraSubscriber> ir_camera_sub_;
   rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pointcloud_pub_;
+
+  cv::Mat depth_img_;
+  cv::Mat ir_img_;
+  rclcpp::Time depth_img_stamp_;
+  rclcpp::Time ir_img_stamp_;
+  std::string camera_frame_;
+  double fx_;  // Focal length in x
+  double fy_;  // Focal length in y
+  double cx_;  // Optical center in x
+  double cy_;  // Optical center in y
 
   cv::Mat gripper_mask_;
   sensor_msgs::msg::PointCloud2 pointcloud_msg_;
